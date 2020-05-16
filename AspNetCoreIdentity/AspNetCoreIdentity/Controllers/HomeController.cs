e6af79e98dcd09c100b97221904d3dda71f1b61b
Anderson <anderson@prodwin.com.br>
2020-05-16 20:25:05 -0300

    AspNetCore MVC com Identity
    AspNetCore Básica utilizando o Identity

---
+++ AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+using AspNetCoreIdentity.Extensions;
+using KissLog;
+using System;
+
+namespace AspNetCoreIdentity.Controllers {
+
+	[Authorize]
+	public class HomeController: Controller {
+
+		private readonly ILogger _logger;
+
+		public HomeController(ILogger logger) {
+			_logger = logger;
+		}
+
+		/*Nessa classe o controller está como [Authorize], isso bloqueia todo mundo acessar essa controller (Views)
+		 * porém a Home precisa ser acessado por todos, então liberados com o [AllowAnonymous]
+		*/
+
+		[AllowAnonymous] // permite todo mundo acessar
+		public IActionResult Index() {
+			_logger.Trace("Usuario acessou a home!");
+
+			return View();
+		}
+
+		//[Authorize] // só funciona para usuários que fizerem login
+		public IActionResult Privacy() {
+
+
+			throw new System.Exception("ERRO");
+			return View();
+		}
+
+		/*Autorização é diferente de autenticação
+		 * Auenticação: Verifica se o usuário está logado
+		 * Autorização: Verifica se o usuário tem permissão de acessar
+		*/
+		[Authorize(Roles = "Admin, Gestor")]
+		public IActionResult Secret() {
+
+			try {
+
+				throw new Exception("Algo deu errado!");
+
+			}catch(Exception ex) {
+				_logger.Error(ex);
+				throw;
+			}
+
+			return View();
+		}
+
+		[Authorize(Policy = "PodeExcluir")]
+		public IActionResult SecretClaim() {
+
+			return View("Secret");
+		}
+
+		[Authorize(Policy = "PodeEscrever")]
+		public IActionResult SecretClaimGravar() {
+
+			return View("Secret");
+		}
+
+		[ClaimsAuthorize("Produtos", "Ler")]
+		public IActionResult ClaimsCustom() {
+
+			return View("Secret");
+		}
+
+		[Route("erro/{id:length(3,3)}")]
+		public IActionResult Error(int id) {
+
+			var modelErro = new ErrorViewModel();
+			if(id == 500) {
+				modelErro.Mensagem = "Ocorreu um erro! Tente novamente mais tarde ou contate noso suporte.";
+				modelErro.Titulo = "Ocorreu um erro!";
+				modelErro.ErrorCode = id;
+			} else if (id == 404) {
+
+				modelErro.Mensagem = "A página que está procurando não existe! <br />Em caso de dúvidas entre em contato com nosso suporte";
+				modelErro.Titulo = "Ocorreu um erro!";
+				modelErro.ErrorCode = id;
+
+			} else if(id == 403) {
+
+				modelErro.Mensagem = "Você não tem permissão para fazer isto.";
+				modelErro.Titulo = "Acesso negado";
+				modelErro.ErrorCode = id;
+
+			} else {
+
+				return StatusCode(404);
+			}
+
+			return View("Error", modelErro);
+		}
+	}
+}
---
+++ AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
@@ -0,0 +1,85 @@
+﻿using AspNetCoreIdentity.Config;
+using AspNetCoreIdentity.Extensions;
+using KissLog;
+using KissLog.Apis.v1.Listeners;
+using KissLog.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreIdentity {
+	public class Startup {
+		public IConfiguration Configuration { get; }
+		
+		//Vem assim por default
+		//public Startup(IConfiguration configuration) {
+		//	Configuration = configuration;
+		//}
+
+		public Startup(IHostingEnvironment hostingEnvironment) {
+			//Configuração para setar o ambiente de desenvolvimento
+				var builder = new ConfigurationBuilder()
+				.SetBasePath(hostingEnvironment.ContentRootPath)
+				.AddJsonFile("appsettings.json", true, true)
+				.AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true)
+				.AddEnvironmentVariables();
+
+			if(hostingEnvironment.IsProduction()) {
+
+				/*Copiar a conenction string da produção appsettings.Production.json
+				 clicar botão direito no projeto e "Manage User Secrets", colar a connection string nesse arquivo secrets.json
+				 */
+				builder.AddUserSecrets<Startup>();
+			}
+
+			Configuration = builder.Build();
+		}
+
+		public void ConfigureServices(IServiceCollection services) {
+
+			services.AddIdentityConfig(Configuration);
+			services.AddAuthorizationConfig();
+			services.ResolveDependencies();
+
+			services.AddMvc(options => {
+				options.Filters.Add(typeof(AuditoriaFilter));
+
+			}).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+			if(env.IsDevelopment()) {
+
+				app.UseDeveloperExceptionPage();
+
+			} else {
+
+				//app.UseExceptionHandler("/Home/Error");
+
+				app.UseExceptionHandler("/erro/500");
+				app.UseStatusCodePagesWithRedirects("/erro/{0}");
+
+				app.UseHsts();
+			}
+
+			app.UseKissLogMiddleware();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+
+			//Para autilizar o Identity
+			app.UseAuthentication();
+
+			app.UseMvc(routes => {
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+			 
+			LogConfig.RegisterKissLogListeners(Configuration);
+		}
+
+	}
+}
